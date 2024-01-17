b65431774b839b11a1c88d53744e3c319e315118
Flavien FOUQUERAY <65244389+ffouqueray@users.noreply.github.com>
2024-01-17 20:23:22 +0000

    exercices 5

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -7,11 +7,9 @@
 //
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let v = vec![10, 20, 30, 40]; // TODO: declare your vector here with the macro for vectors
 
     (a, v)
 }
@@ -24,5 +22,5 @@ mod tests {
     fn test_array_and_vec_similarity() {
         let (a, v) = array_and_vec();
         assert_eq!(a, v[..]);
-    }
+    } 
 }
